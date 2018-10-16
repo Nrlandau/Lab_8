583d3dd01543a0c6ea32f6ccdbcea0c3ea26c2cf
Nicholas Landau <nrlandau@gmail.com>
2018-10-16 11:04:26 -0400

    Created File Input

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Lab8
 {
@@ -10,14 +11,34 @@ namespace Lab8
     }
     class Program
     {
+        static void MakePeople(StreamReader file, Person[] people)
+        {
+            for(int i =0; i < people.Length; i++)
+            {
+                people[i].name = file.ReadLine();
+                people[i].hometown = file.ReadLine();
+                people[i].favFood = file.ReadLine();
+            }
+        }
+        static void DisplayPeople(Person[] people)
+        {
+            int i =1;
+            foreach(Person p in people)
+            {
+                System.Console.WriteLine("Name {3}:{0}\nHometown {3}:{1}\nFavoriteFood {3}:{2}",p.name,p.hometown,p.favFood,i);
+                i++;
+            }
+        }
         static void Main(string[] args)
         {
-            int totalLength = 5;
+            string filePath = "C:\\Code\\Files\\Lab8\\People.txt";
+            StreamReader sr = new StreamReader(filePath);
+            int totalLength = int.Parse(sr.ReadLine());
             Person [] people = new Person[totalLength];
-            for(int i = 0; i < totalLength; i++)
-            {
-                
-            } 
+            MakePeople(sr, people);
+            DisplayPeople(people);
+
+
 
         }
     }
