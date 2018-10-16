4b009b6d2af9b80c14dc8c0ad60d97e802e68f24
Nicholas Landau <nrlandau@gmail.com>
2018-10-16 11:56:39 -0400

    Created DisplayPerson Function

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Lab8
 {
@@ -29,6 +30,56 @@ namespace Lab8
                 i++;
             }
         }
+        static void DisplayPerson(Person p , int index)
+        {
+            string moreiInfo ="";
+            bool isMore = true;
+            System.Console.WriteLine("Student {0} is {1}. What would you like to know about {1}(enter \"Hometown\" or \"Favorite food\")",index,p.name);
+            while(isMore)
+            {
+                moreiInfo = System.Console.ReadLine();
+                if(moreiInfo.ToLower() == "hometown")
+                {
+                    System.Console.Write("{0} is from {1} ",p.name,p.hometown);
+                }
+                else if(Regex.IsMatch(moreiInfo.ToLower(), @"food"))
+                {
+                    System.Console.Write("{0} likes {1} ",p.name,p.favFood);
+                }
+                else
+                {
+                    System.Console.WriteLine("That data does not exist, please try again");
+                    continue;
+                }
+                moreiInfo = "";
+                while(!Regex.IsMatch(moreiInfo,"^[yYNn]"))
+                {
+                    System.Console.WriteLine("Would you like to know more?(yes or no):");
+                    moreiInfo = System.Console.ReadLine();
+                }
+                if(Regex.IsMatch(moreiInfo,@"^[yY]"))
+                {
+                    System.Console.WriteLine("What would you like to know about {1}(enter \"Hometown\" or \"Favorite food\")");
+                }
+                else
+                    isMore = false;
+            }
+        }
+        static void UserInputLoop(Person[] people)
+        {
+            int stuNum;
+            try{
+                System.Console.WriteLine("Enter a number 1-{0}", people.Length );
+                stuNum = int.Parse(System.Console.ReadLine());
+                DisplayPerson(people[stuNum-1],stuNum);
+
+            }
+            catch (Exception)
+            {
+                System.Console.WriteLine("ERROR");
+            }
+
+        }
         static void Main(string[] args)
         {
             string filePath = "C:\\Code\\Files\\Lab8\\People.txt";
@@ -36,7 +87,7 @@ namespace Lab8
             int totalLength = int.Parse(sr.ReadLine());
             Person [] people = new Person[totalLength];
             MakePeople(sr, people);
-            DisplayPeople(people);
+            UserInputLoop(people);
 
 
 
