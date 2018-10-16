9c3c1697578efe52e466bc134789d95df968a1b0
Nicholas Landau <nrlandau@gmail.com>
2018-10-16 10:44:46 -0400

    Start Lab

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab8
+{
+    struct Person
+    {
+        public string name;
+        public string hometown;
+        public string favFood;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalLength = 5;
+            Person [] people = new Person[totalLength];
+            for(int i = 0; i < totalLength; i++)
+            {
+                
+            } 
+
+        }
+    }
+}
